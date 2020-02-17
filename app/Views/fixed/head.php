8eeb1461703aa8306ea7f9c8af97290878c232ea
nabadamdam <61154727+nabadamdam@users.noreply.github.com>
2020-02-17 16:33:04 +0100

    Update head.php

--- app/Views/fixed/head.php
+++ app/Views/fixed/head.php
@@ -4,11 +4,11 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Savory &mdash; Free Website Template, Free HTML5 Template by GetTemplates.co</title>
+	<title>Savory restaurant</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Website Template by GetTemplates.co" />
-	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
-	<meta name="author" content="GetTemplates.co" />
+	<meta name="description" content="Savory restaurant the best place for food"/>
+	<meta name="keywords" content="Savory,restaurant,best,place,food"/>
+	<meta name="author" content="Nikola Riorovic"/>
 
   	<!-- Facebook and Twitter integration -->
 	<meta property="og:title" content=""/>
